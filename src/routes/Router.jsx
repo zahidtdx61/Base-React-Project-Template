f82b746b2a63e1986a45f3304debf8ec17bd40a3
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-03-31 11:22:44 +0600

    added mainLayout and home page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout/MainLayout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world</div>,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
